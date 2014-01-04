47b1ee52ab26ae5d42a6e0156ebf5927813bfc27
ofrioster <ofrioster@gmail.com>
2014-01-04 19:05:47 +0200

    main update
    Signed-off-by: ofrioster <ofrioster@gmail.com>

--- test1/src/mainTest.java
+++ test1/src/mainTest.java
@@ -1,9 +1,58 @@
+import java.io.*;
+import java.net.*;
+
 
 public class mainTest {
-	 public static void main(String[] args) {
+	 public static void main(String[] args) throws IOException{
 		 System.out.println("work");
 
-	 }
-	
+		        ServerSocket lpServerSocket = null;
+		 
+		        // Get port
+		        int port = Integer.decode(args[0]).intValue();
+		        
+		        // Listen on port
+		        try {
+		            lpServerSocket = new ServerSocket(port);
+		        } catch (IOException e) {
+		            System.out.println("Couldn't listen on port " + port);
+		            System.exit(1);
+		        }
+		        
+		        System.out.println("Listening...");
+		        
+		        // Waiting for a client connection
+		        Socket lpClientSocket = null;
+		        try {
+		            lpClientSocket = lpServerSocket.accept();
+		        } catch (IOException e) {
+		            System.out.println("Failed to accept...");
+		            System.exit(1);
+		        }
+		        
+		        System.out.println("Accepted connection from client!");
+		        System.out.println("The client is from: " + lpClientSocket.getInetAddress() + ":" + lpClientSocket.getPort());
+		        
+		        // Read messages from client
+		        BufferedReader in = new BufferedReader(new InputStreamReader(lpClientSocket.getInputStream()));
+		        String msg;
+		 
+		        while ((msg = in.readLine()) != null)
+		        {
+		            System.out.println("Received from client: " + msg);
+		            if (msg.equals("bye"))
+		            {
+		                System.out.println("Client sent a terminating message");
+		                break;
+		            }
+		        }
+		        
+		        System.out.println("Client disconnected - bye bye...");
+		        
+		        lpServerSocket.close();
+		        lpClientSocket.close();
+		        in.close();
+		        
+		    }
 
 }
