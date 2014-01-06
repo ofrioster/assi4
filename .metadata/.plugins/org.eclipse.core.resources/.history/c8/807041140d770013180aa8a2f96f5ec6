d93451923ff46a40d6e8ad28390b5f3ed299a880
ofri <ofrioster@gmail.com>
2014-01-06 22:02:56 +0200

    add token

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/c8/807041140d770013180aa8a2f96f5ec6
@@ -0,0 +1,7 @@
+import java.io.BufferedReader;
+
+
+public interface StompTokenizer {
+	 public StompFrame getFrame(BufferedReader br);
+
+}
---
+++ test1/src/StompTokenizerInterface.java
@@ -0,0 +1,7 @@
+import java.io.BufferedReader;
+
+
+public interface StompTokenizerInterface {
+	 public StompFrame getFrame(BufferedReader br);
+
+}
